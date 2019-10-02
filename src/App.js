a19bd0163e8f8f6b00a062ad1ec9945ff24926c0
AndreCaseiro <andremfcaseiro@gmail.com>
2019-10-02 17:22:06 +0100

    links

--- src/App.js
+++ src/App.js
@@ -1,58 +1,54 @@
 import React, { Component } from 'react';
-import ReactDOM from 'react-dom'
-import { Link, Route } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import './App.css';
 import "./assets/css/style.css"
-import Alojamento from './Alojamento'
-const routing = (
-  <Router>
-    <div>
-    <ul>
-      <li>
-        <Link to="/Alojamento">Alojamento</Link>
-      </li>
-    </ul>
-      <Route exact path="/Alojamento" component={Alojamento} />
-    </div>
-  </Router>
-)
-ReactDOM.render(routing, document.getElementById('root'))
 
-class App extends Component {
-render(){
-  return (
-    <div className="App">
-      <header className="App-header">
-        <header class="top-area">
-				<div class="container">
-						<div class="col-sm-2">
-							<div class="logo">
-								<a href="index.html">
-									Quintais do <span>Caneiro</span>
-								</a>
-							</div>
-						<div class="col-sm-10">
-							<div class="main-menu">
-								<div class="navbar-header">
-								</div>
-									<ul id="menu" class="nav navbar-nav navbar-right ">
+  class App extends Component {
+    render(){
+        return (
+          <Router>
+          <div className="App">
+          <header className="App-header">
+          <header class="top-area">
+          <div class="container">
+          <div class="col-sm-2">
+          <div class="logo">
+          <a href="index.html">
+          Quintais do <span>Caneiro</span>
+          </a>
+            </div>
+            <span className="input-group-btn">
+            <Link to="/Alojamento" > Alojamento</Link>
+            </span>
 
-										<li>
-
-											<button class="book-btn">book now</button>
-										</li>
-									</ul>
-						</div>
-					</div>
-					<div class="home-border"></div>
-				</div>
-			</div>
-		</header>
-      </header>
-    </div>
-  );
-}
-}
-
-export default App;
+          <div class="col-sm-10">
+          <div class="main-menu">
+          <div class="navbar-header">
+          </div>
+          <ul id="menu" class="nav navbar-nav navbar-right ">
+          <li>
+          <button class="book-btn">book now</button>
+          </li>
+          <div class="texto">
+          Os Quintais do Caneiro são um conjunto de propriedades rústicas centenárias situada nas margens do rio Ceira, no concelho de Miranda do Corvo, num belíssimo enquadramento paisagístico e cultural, que distam cerca de 10 Km da cidade de Coimbra.
+          Os proprietários decidiram apostar neste projeto de turismo rural associado à agricultura biológica. As mobílias foram criteriosamente escolhidas, adequando-se ao ambiente rústico e sóbrio da casa. Os quartos são muito confortáveis, com ar condicionado,
+          a cozinha totalmente equipada, muito ampla, manteve o forno tradicional a lenha. Na eira, espaço amplo, podemos desfrutar de todo o conforto, privacidade e magníficas vistas sobre o rio Ceira.
+          A casa está rodeada de terrenos agrícolas, onde a horta biológica, as ervas aromáticas e a piscina biológica fazem as delícias de todos aqueles que nos visitam.
+          </div>
+          </ul>
+          </div>
+          </div>
+          <div class="home-border"></div>
+          </div>
+          </div>
+          </header>
+          </header>
+          </div>
+          </Router>
+        );
+      }
+    }
+    
+    export default App;
+    
--- src/index.js
+++ src/index.js
@@ -3,9 +3,25 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { Link, Route } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+import Alojamento from './Alojamento'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
+const routing = (
+  <Router>
+  <ul>
+  <li>
+  <Link to="/Alojamento">Alojamento</Link>
+  </li>
+  <li>
+    <Link to="/">Home</Link>
+    </li>
+  </ul>
+  <Route exact path="/" component={App} />
+  <Route exact path="/Alojamento" component={Alojamento} />
+  </Router>
+  )
+  ReactDOM.render(routing, document.getElementById('root'))
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
