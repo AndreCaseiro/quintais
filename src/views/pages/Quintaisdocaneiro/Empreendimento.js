d3aa76f76e9342276919e92e0ee8b70939f1c5f5
AndreCaseiro <andremfcaseiro@gmail.com>
2019-10-17 14:10:56 +0100

    get done

--- src/views/pages/Quintaisdocaneiro/Empreendimento.js
+++ src/views/pages/Quintaisdocaneiro/Empreendimento.js
@@ -14,57 +14,35 @@ import "./../../css/responsive.css";
 import "./../../css/style.css";
 import "@brainhubeu/react-carousel/lib/style.css";
 
-
 const axios = require('axios').default;
 
-// const getPage = (callback) => {
-//   axios.get('http://admin.quintaisdocaneiro.com/api/gets.php?act=getPage&area=entretenimento')
-//           .then(function (response) {
-//             // handle success
-//             callback(response);
-//           })
-//           .catch(function (error) {
-//             // handle error
-//             console.log(error);
-//           })
-//         }
-        
-//   const renderData =(array) => {
-//     console.log('fdp', array);
-//     if(!array) return null;
-//     return (
-//       <div>
-//           <p>{array[0].name}</p>
-//       </div>
-//     );
-//   }
-
-//   const handleData = (response) => { 
-//     return response;
-//   }
-
-const getPage = () => {
-  axios.get('http://admin.quintaisdocaneiro.com/api/gets.php?act=getPage&area=entretenimento')
-    .then(function (response) {
-      // handle success
-      handleData(response);
-    })
-    .catch(function (error) {
-      // handle error
-      console.log(error);
-    })
-}
-
-const handleData = (response) => {
-  console.log(response);
-  return response;
-}
-
 class Empreendimento extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      info: null,
+    };
+  }
+    componentDidMount(){
+	  this.getAdminContent();
+    }
+
+    getAdminContent = () => {
+      if(this.state.info === null){
+        console.log(this.state)
+        axios.get('http://admin.quintaisdocaneiro.com/api/gets.php?act=getPage&area=entretenimento')
+          .then((response) => {
+            this.setState({
+              info: Object.values(response.data),
+            })
+          })
+        }
+    }
+
+
   render() {
-    console.log('1', getPage());
-    console.log('2', getPage());
-    console.log('3', getPage());
+    const { info } = this.state;
+
     return(
       <div className="Header">
       <Header></Header>
@@ -86,7 +64,12 @@ class Empreendimento extends Component {
     </section>
     <div className="content">
     <h1>
-    {handleData()}
+    {info && info.map(obj => (
+                <div>
+                <p>{obj.title}</p>
+                <p>{obj.text}</p>
+                </div>
+              ))}
     </h1>
     </div>
     <div className="Footer"><Footer></Footer></div>
