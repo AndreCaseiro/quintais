f9356146a6a5343cc3fa06122d84fb8c9c9f7bd1
AndreCaseiro <andremfcaseiro@gmail.com>
2019-11-28 17:14:54 +0000

    mobile responsive done

--- src/views/components/Header/index.js
+++ src/views/components/Header/index.js
@@ -7,8 +7,15 @@ import DropdownButton from 'react-bootstrap/DropdownButton'
 import ReactFlagsSelect from 'react-flags-select';
 import 'react-flags-select/css/react-flags-select.css';
 import { withTranslation, Trans } from 'react-i18next';
+import { slide as Menu } from 'react-burger-menu'
 
 class Header extends Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {
+        };
+    }
     componentDidMount(){
         var nav = document.getElementById('nav');
                 window.onscroll = function(){
@@ -29,11 +36,14 @@ class Header extends Component {
     }
     showSettings (event) {
         event.preventDefault();
-      }
+    }
 
     render(){
-        
-          
+
+    var isMenuOpen = function(state){
+        console.log(state.isOpen);
+    }
+
         const route= this.props.match.params.lang ? this.props.match.params.lang : "pt";
         return(
             <React.Fragment>
@@ -51,18 +61,20 @@ class Header extends Component {
                     <div className="main-menu">
                     <div className="sticky">
                         <div className="navbar">
+                        <div className="menuMobile">
+                        <Menu onStateChange={ isMenuOpen }>
                             <ul className="nav navbar-nav navbar-right">
                                 <li>
-                                <DropdownButton id="dropdown-basic-button" title={this.props.i18n.t('Quintais')}>
-                                    <Dropdown.Item href={`/${route}/pages/Empreendimento`}><Trans i18nKey="Empreendimento"></Trans></Dropdown.Item>
-                                    <Dropdown.Item href={`/${route}/Galeria`}><Trans i18nKey="Galeria"></Trans></Dropdown.Item>
-                                </DropdownButton>
-                                    </li>
+                                    <DropdownButton id="dropdown-basic-button" title={this.props.i18n.t('Quintais')}>
+                                        <Dropdown.Item href={`/${route}/pages/Empreendimento`}><Trans i18nKey="Empreendimento"></Trans></Dropdown.Item>
+                                        <Dropdown.Item href={`/${route}/Galeria`}><Trans i18nKey="Galeria"></Trans></Dropdown.Item>
+                                    </DropdownButton>
+                                </li>
                                 <li>
-                                <DropdownButton id="dropdown-basic-button" title={this.props.i18n.t('Alojamento')}>
-                                    <Dropdown.Item href={`/${route}/pages/localizacao`}><Trans i18nKey="Localizacao"></Trans></Dropdown.Item>
-                                    <Dropdown.Item href={`/${route}/pages/comodidades`}><Trans i18nKey="Comodidades"></Trans></Dropdown.Item>
-                                </DropdownButton>
+                                    <DropdownButton id="dropdown-basic-button" title={this.props.i18n.t('Alojamento')}>
+                                        <Dropdown.Item href={`/${route}/pages/localizacao`}><Trans i18nKey="Localizacao"></Trans></Dropdown.Item>
+                                        <Dropdown.Item href={`/${route}/pages/comodidades`}><Trans i18nKey="Comodidades"></Trans></Dropdown.Item>
+                                    </DropdownButton>
                                 </li>
                                 <li>
                                     <DropdownButton id="dropdown-basic-button" title={this.props.i18n.t('Natureza')}>
@@ -71,21 +83,60 @@ class Header extends Component {
                                     </DropdownButton>
                                 </li>
                                 <li>
-                                <a  href={`/${route}/pages/reservas`}><Trans i18nKey="Reservas"></Trans></a>
+                                    <a href={`/${route}/pages/reservas`}><Trans i18nKey="Reservas"></Trans></a>
                                 </li>
                                 <li>
-                                <Link to={`/${route}/contactos`}><Trans i18nKey="Contactos"></Trans></Link>
+                                    <Link to={`/${route}/contactos`}><Trans i18nKey="Contactos"></Trans></Link>
                                 </li>
                                 <li>
-                                <ReactFlagsSelect className="flags"
-                                    defaultCountry={route.toUpperCase()}
-                                    countries={["ES", "GB", "FR", "PT"]}
-                                    showSelectedLabel={false}
-                                    showOptionLabel={false}
-                                    onSelect={this.changeCountry}
-                                />
+                                    <ReactFlagsSelect className="flags"
+                                        defaultCountry={route.toUpperCase()}
+                                        countries={["ES", "GB", "FR", "PT"]}
+                                        showSelectedLabel={false}
+                                        showOptionLabel={false}
+                                        onSelect={this.changeCountry}
+                                    />
                                 </li>
                             </ul>
+                            </Menu>
+                            </div>
+                            <div className="menuDesktop">
+                            <ul className="nav navbar-nav navbar-right">
+                                <li>
+                                    <DropdownButton id="dropdown-basic-button" title={this.props.i18n.t('Quintais')}>
+                                        <Dropdown.Item href={`/${route}/pages/Empreendimento`}><Trans i18nKey="Empreendimento"></Trans></Dropdown.Item>
+                                        <Dropdown.Item href={`/${route}/Galeria`}><Trans i18nKey="Galeria"></Trans></Dropdown.Item>
+                                    </DropdownButton>
+                                </li>
+                                <li>
+                                    <DropdownButton id="dropdown-basic-button" title={this.props.i18n.t('Alojamento')}>
+                                        <Dropdown.Item href={`/${route}/pages/localizacao`}><Trans i18nKey="Localizacao"></Trans></Dropdown.Item>
+                                        <Dropdown.Item href={`/${route}/pages/comodidades`}><Trans i18nKey="Comodidades"></Trans></Dropdown.Item>
+                                    </DropdownButton>
+                                </li>
+                                <li>
+                                    <DropdownButton id="dropdown-basic-button" title={this.props.i18n.t('Natureza')}>
+                                        <Dropdown.Item href={`/${route}/pages/actividades`}><Trans i18nKey="AtividadesExterior"></Trans></Dropdown.Item>
+                                        <Dropdown.Item href={`/${route}/pages/ocaminho`}><Trans i18nKey="Caminho"></Trans></Dropdown.Item>
+                                    </DropdownButton>
+                                </li>
+                                <li>
+                                    <a href={`/${route}/pages/reservas`}><Trans i18nKey="Reservas"></Trans></a>
+                                </li>
+                                <li>
+                                    <Link to={`/${route}/contactos`}><Trans i18nKey="Contactos"></Trans></Link>
+                                </li>
+                                <li>
+                                    <ReactFlagsSelect className="flags"
+                                        defaultCountry={route.toUpperCase()}
+                                        countries={["ES", "GB", "FR", "PT"]}
+                                        showSelectedLabel={false}
+                                        showOptionLabel={false}
+                                        onSelect={this.changeCountry}
+                                    />
+                                </li>
+                            </ul>
+                            </div>
                         </div>
                         </div>
                     </div>
@@ -96,6 +147,6 @@ class Header extends Component {
             </header>
             </React.Fragment>
         );
-    }
+}
 }
 export default withTranslation('translation')(withRouter(Header));
